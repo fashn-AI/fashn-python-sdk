36bc09dfb2c795596b21f63a673a3fca502263e8
Renan Lopes Ferreira <relferreira@gmail.com>
2025-10-13 17:46:33 -0300

    feat: subscribe method

--- src/fashn/types/__init__.py
+++ src/fashn/types/__init__.py
@@ -5,3 +5,5 @@ from __future__ import annotations
 from .prediction_run_params import PredictionRunParams as PredictionRunParams
 from .prediction_run_response import PredictionRunResponse as PredictionRunResponse
 from .prediction_status_response import PredictionStatusResponse as PredictionStatusResponse
+from .prediction_subscribe_params import PredictionSubscribeParams as PredictionSubscribeParams
+from .prediction_subscribe_response import PredictionSubscribeResponse as PredictionSubscribeResponse
